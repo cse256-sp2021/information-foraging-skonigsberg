cc666bee9ddff0201212941c72a4a8bd8b39332c
samanthakonigsberg <skonigsberg@wustl.edu>
2021-03-14 15:37:40 -0500

    submission2

--- scripts/ts/dynamic-dom/stick.js
+++ scripts/ts/dynamic-dom/stick.js
@@ -4,15 +4,22 @@ window.onscroll = function() {myFunction()};
 // Get the header
 var header = document.getElementById("main-menu-container");
 
-
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
+
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-if (window.pageYOffset > sticky) {
+  var x = document.getElementById("mturk-top-banner-drop-down-content");
+  if (window.getComputedStyle(x).display === "none") {
+  header.classList.add("sticky2");
+} else{
+  header.classList.remove("sticky2");
+}
+
+if (window.pageYOffset > sticky)  {
   header.classList.add("sticky");
-} else {
+} else{
   header.classList.remove("sticky");
 }
 }
